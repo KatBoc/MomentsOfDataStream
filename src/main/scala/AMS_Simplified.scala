9f976fa5672de13bd3afa000b03b4d9636384715
Katarzyna Bocian (239911) <katbocian98@gmail.com>
2023-07-05 14:01:47 +0200

    Updated AMS
    Updated simplified AMS and AMS algorithm.

--- src/main/scala/AMS_Simplified.scala
+++ src/main/scala/AMS_Simplified.scala
@@ -1,43 +1,28 @@
 import scala.util.Random
 
-/**
- * AMS_Simplified implements the Alon-Matias-Szegedy sketch algorithm
- * to estimate the moments of the distribution of elements in a stream of data.
- *
- * @param streamLength Length of the data stream.
- * @param numSamples Number of random indices to sample.
- */
-class AMS_Simplified(streamLength: Int, numSamples: Int) {
-  private val randomPositions: Array[Int] = Array.fill(numSamples)(Random.nextInt(streamLength))
+class AMS_Simplified(streamLength: Int, numSamples: Int = 60) {
+  private val randomPositions: Set[Int] = (1 to numSamples).map(_ => Random.nextInt(streamLength) + 1).toSet
   private val counts: Array[Int] = new Array[Int](numSamples)
   private val words: Array[String] = new Array[String](numSamples)
-  private var position = 0
+  private var position : Int = 0
 
-  /**
-   * Adds a word to the sketch.
-   *
-   * @param word The word to add.
-   */
   def add(word: String): Unit = {
     position += 1
-    if (randomPositions.contains(position))
-      {
-        val i = randomPositions.indexOf(position)
-        words(i) = word
-        counts(i) += 1
-      } else if (words.contains(word)) {
-      counts(words.indexOf(word)) += 1
+    if (randomPositions.contains(position)) {
+      val positionIndex = randomPositions.toList.indexOf(position)
+      words(positionIndex) = word
+      counts(positionIndex) += 1
+    } else {
+      val wordIndex = words.indexOf(word)
+      if (wordIndex >= 0) {
+        counts(wordIndex) += 1
+      }
     }
   }
 
-  /**
-   * Estimates the k-th moment of the data stream's distribution.
-   *
-   * @param momentOrder The order of the moment to estimate.
-   * @return The estimated value of the k-th moment.
-   */
-  def estimateMoment(momentOrder: Int): BigInt = {
-    val momentEstimate = counts.map(x => BigInt(math.pow(x, momentOrder).toInt) - BigInt(math.pow(x - 1, momentOrder).toInt)).sum
-    BigInt(streamLength / numSamples) * momentEstimate
+  def estimateMoment(k: Int): BigInt = {
+    val momentEstimate = counts.map(x => (BigInt(math.pow(x, k).toInt) - BigInt(math.pow(x - 1, k).toInt)))
+    val mk = BigInt(streamLength) / BigInt(numSamples) * momentEstimate.sum
+    mk
   }
 }
--- src/main/scala/Usage.scala
+++ src/main/scala/Usage.scala
@@ -1,23 +1,27 @@
 import scala.io.Source
+import scala.util.Random.nextInt
 
 object Usage {
 
   private def txtFileToSeq(filePath: String): Seq[String] = {
     val fileSource = Source.fromFile(filePath)
-    val words = fileSource.getLines
-      .flatMap(_.split("\\W+"))
-      .map(_.toLowerCase)
-      .filter(_.nonEmpty) // Filter out empty strings
-      .toSeq
+    val words = fileSource.mkString.toLowerCase().split("\\W+").filter(_.nonEmpty).toSeq
     fileSource.close()
     words
   }
 
-  private def estimate_AMS_Simplified(words: Seq[String], k: Int, r: Int): Int = {
-    val ams = new AMS_Simplified(words.length, r)
+  private def estimate_AMS_Simplified(words: Seq[String], k: Int): BigInt = {
+    val amsS = new AMS_Simplified(words.length)
+    words.foreach(amsS.add)
+    val kthMomentEstimate = amsS.estimateMoment(k)
+    kthMomentEstimate
+  }
+
+  private def estimate_AMS(words: Seq[String], k: Int, epsilon: Double, delta: Double): BigInt = {
+    val ams = new AMS_Algorithm(k, epsilon, delta)
     words.foreach(ams.add)
     val kthMomentEstimate = ams.estimateMoment(k)
-    kthMomentEstimate.toInt
+    kthMomentEstimate
   }
 
   private def ExactMoment(k: Int, words: Seq[String]) = {
@@ -28,26 +32,34 @@ object Usage {
   def main(args: Array[String]): Unit = {
     val filePath = "C:/Users/KatarzynaBocian(2399/Desktop/BDA/Stream programming/A/Stream_programming_assigments/src/canterbury-corpus-master/canterbury/alice29.txt"
     val words = txtFileToSeq(filePath)
-    val M0 = ExactMoment(0, words)
-    val EsM0 = estimate_AMS_Simplified(words, 0, 60)
-    val M1 = ExactMoment(1, words)
-    val EsM1 = estimate_AMS_Simplified(words, 1, 60)
-    val M2 = ExactMoment(2, words)
-    val EsM2 = estimate_AMS_Simplified(words, 2, 60)
-    val M3 = ExactMoment(3, words)
-    val EsM3 = estimate_AMS_Simplified(words, 3, 60)
 
-//    println("Exact 1st: " + M1)
-//    println("Estimate_Simply 1st: " + EsM1)
-//    println("Rate: " + EsM1.toDouble / M1.toDouble)
-//    println(" ")
-    println("Exact 2nd: " + M2)
-    println("Estimate_Simply 2st: " + EsM2)
-    println("Rate: " + EsM2.toDouble / M2.toDouble)
-    println(" ")
-    println("Exact 3nd: " + M3)
-    println("Estimate_Simply 3st: " + EsM3)
-    println("Rate: " + EsM3.toDouble/M3.toDouble)
-    println(" ")
+    // estimates to be within 1% of the true value with 95% confidence.
+    val epsilon = 0.1
+    val delta = 0.1
+
+/*    val M0 = ExactMoment(0, words)
+    val EsM0 = estimate_AMS_Simplified(words, 0)
+    val M1 = ExactMoment(1, words)
+    val EsM1 = estimate_AMS_Simplified(words, 1)*/
+
+    val M2 = ExactMoment(2, words)
+    val EsM2 = estimate_AMS_Simplified(words, 2)
+    val EM2 = estimate_AMS(words, 2, epsilon, delta)
+    val M3 = ExactMoment(3, words)
+    val EsM3 = estimate_AMS_Simplified(words, 3)
+    val EM3 = estimate_AMS(words, 3, epsilon, delta)
+
+
+    // Printing header
+
+    printf("| %2s | %15s | %20s | %15s | %15s | %15s |\n", "k", "Exact Moment", "Simplify AMS Estimate", "Error Rate", "AMS Estimate", "Rate AMS/E")
+    println("|----|-----------------|-----------------------|-----------------|-----------------|-----------------|")
+
+    // Printing rows
+/*
+    printf("| %2d | %15d | %21d | %15s | %15s | %15s |\n",     0, M0, EsM0, "N/A", "N/A", "N/A")
+    printf("| %2d | %15d | %21d | %15.4f | %15s | %15s |\n",   1, M1, EsM1, EsM1.toDouble / M1, "N/A", "N/A")*/
+    printf("| %2d | %15d | %21d | %15.4f | %15d | %15.2f |\n", 2, M2, EsM2, (math.abs(EsM2.toDouble - M2.toDouble) / M2.toDouble), EM2, (math.abs(EM2.toDouble - M2.toDouble) / M2.toDouble))
+    printf("| %2d | %15d | %21d | %15.4f | %15d | %15.2f |\n", 3, M3, EsM3, (math.abs(EsM3.toDouble - M3.toDouble) / M3.toDouble), EM3, (math.abs(EM3.toDouble - M3.toDouble) / M3.toDouble))
   }
 }
